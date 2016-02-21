187d13aad99c314aa383a5ec4f20514a0c201c03
Simon Hoenscheid <simon.hoenscheid@netways.de>
2016-02-21 20:54:37 +0100

    fixed hiera-eyaml-gpg on puppet4, relates to https://tickets.puppetlabs.com/browse/PUP-5953

--- modules/site_module/manifests/profiles/single_puppetmaster.pp
+++ modules/site_module/manifests/profiles/single_puppetmaster.pp
@@ -2,9 +2,9 @@
 class site_module::profiles::single_puppetmaster (
   $puppetdb_address    = hiera('site_module::profiles::single_puppetmaster::puppetdb_address', undef),
   $puppet_config_path  = $settings::confdir,
+  $use_hiera_eyaml_gpg = hiera('site_module::profiles::single_puppetmaster::use_hiera_eyaml_gpg', false),
 ){
   include site_module::params
-
   $major_puppetversion = $site_module::params::major_puppetversion
 
   $hiera_path = $major_puppetversion ? {
@@ -14,6 +14,10 @@ class site_module::profiles::single_puppetmaster (
   $terminus_package = $major_puppetversion ? {
     '3' => 'puppetdb-terminus',
     '4' => 'puppetdb-termini',
+  }
+  $hiera_templatefile = $major_puppetversion ? {
+    '3' => 'site_module/profiles/puppetmaster/hiera.3.yaml.erb',
+    '4' => 'site_module/profiles/puppetmaster/hiera.4.yaml.erb',
   }
 
   class{ 'puppetdb::master::config':
@@ -32,12 +36,13 @@ class site_module::profiles::single_puppetmaster (
   }
 
   file{ $hiera_path:
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => "puppet:///modules/site_module/profiles/puppetmaster/hiera.${major_puppetversion}.yaml",
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($hiera_templatefile),
   }
+
   file{ "${puppet_config_path}/autosign.conf":
     ensure => present,
     owner  => 'root',
