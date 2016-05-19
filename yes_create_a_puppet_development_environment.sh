44518dc9459c738e52ac1d574ddb56c8a3cf171b
Simon Hoenscheid <simon.hoenscheid@netways.de>
2016-05-19 08:50:40 +0200

    edited vagrant creation script, added seperate cleanup script

--- yes_create_a_puppet_development_environment.sh
+++ yes_create_a_puppet_development_environment.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ -f Vagrantfile ]; then
-rm Vagrantfile
-fi
-
+c6='CentOS 6'
+c7='CentOS 7'
 d7='Debian 7'
 d8='Debian 8'
-c7='CentOS 7'
-c6='CentOS 6'
 ora6='Oracle Linux 6'
 ora7='Oracle Linux 7'
 u12='Ubuntu 1204 LTS'
@@ -19,22 +15,38 @@ echo 'Please Enter the Puppetversion you want to use. Type 4 for Puppet4 and 3 f
 read PUPPETVERSION
 
 echo 'Please Enter the OS you want to use on the clientside. Type' 
-echo 'd8 for Debian 8'
-echo 'd7 for Debian 7'
-echo 'c7 for CentOS 7'
 echo 'c6 for CentOS 6'
-echo 'ora7 for Oracle Linux 7'
+echo 'c7 for CentOS 7'
+echo 'd7 for Debian 7'
+echo 'd8 for Debian 8'
 echo 'ora6 for Oracle Linux 6'
+echo 'ora7 for Oracle Linux 7'
 echo 'u12 for Ubuntu 1204 LTS'
 echo 'u14 for Ubuntu 1404 LTS'
 echo 'u16 for Ubuntu 1604 LTS'
 #add input validation
 read OS
 
-
 echo 'Please Enter the number of clients you want to create. possible 1-5'
 #add input validation
 read NUMBER_OF_PUPPETCLIENTS
+
+echo "deleting existing puppetmaster environment"
+vagrant destroy -f
+
+if [ -f Vagrantfile ]; then
+rm Vagrantfile
+fi
+
+for  i in $(ls -1 clients/);
+do
+   echo "deleting all clients in $i"
+   cd clients/$i
+   if [ -f Vagrantfile ]; then
+	   vagrant destroy -f
+   fi
+   cd ../..
+done
 
 if [ "$PUPPETVERSION" == "3" ]; then
   echo "I will build a Puppet development environment for Puppet3 with $NUMBER_OF_PUPPETCLIENTS clients on ${!OS}"
@@ -54,11 +66,21 @@ echo "doing third vagrant run"
 vagrant up
 echo "puppet never runs completely trouble less when setting up a multi node puppetdb setup"
 
+if [ "$PUPPETVERSION" == "3" ]; then
 vagrant ssh puppet -c 'sudo puppet agent -t --server puppet.local'
 vagrant ssh puppet -c 'sudo puppet agent -t --server puppet.local'
 vagrant ssh puppetdb -c 'sudo puppet agent -t --server puppet.local'
 vagrant ssh postgres-puppetdb -c 'sudo puppet agent -t --server puppet.local'
-cd clients/$OSp$PUPPETVERSION
+fi
+
+if [ "$PUPPETVERSION" == "4" ]; then
+vagrant ssh puppet -c 'sudo /opt/puppetlabs/bin/puppet agent -t --server puppet.local'
+vagrant ssh puppet -c 'sudo /opt/puppetlabs/bin/puppet agent -t --server puppet.local'
+vagrant ssh puppetdb -c 'sudo /opt/puppetlabs/bin/puppet agent -t --server puppet.local'
+vagrant ssh postgres-puppetdb -c 'sudo /opt/puppetlabs/bin/puppet agent -t --server puppet.local'
+fi
+
+cd clients/"$OS"p"$PUPPETVERSION"
 for (( i=1; i<=$NUMBER_OF_PUPPETCLIENTS; i++ ))
 do
    echo "building puppetclient 0$i with ${!OS}"
