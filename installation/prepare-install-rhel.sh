59268b56655026268320d7c31056f5496e3af944
Simon Hoenscheid <code@simonhoenscheid.de>
2018-11-29 14:59:47 +0100

    update scripts for puppet 5

--- installation/prepare-install-rhel.sh
+++ installation/prepare-install-rhel.sh
@@ -3,4 +3,3 @@
 sudo yum check-update
 sudo yum install -y bash-completion wget vim
 sudo yum upgrade -y
-sudo wget -O /root/.bashrc https://gist.githubusercontent.com/SimonHoenscheid/b9528d087c2791ab7603/raw/b84197a3378f19d14aa2a9f2d3bde139d13583b1/.bashrc
